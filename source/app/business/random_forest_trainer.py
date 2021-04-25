6bb735ec004d98cdbaa7807ca53458060a3d8872
Hugo <hugo.malamaire06800@gmail.com>
2021-04-25 13:26:01 +0200

    mistakes fixed

--- source/app/business/random_forest_trainer.py
+++ source/app/business/random_forest_trainer.py
@@ -5,7 +5,7 @@ from app.value_object.dataframe import Dataframe
 class RandomForestTrainer:
 
     def __init__(self, machine_learning_adapter: MachineLearningAdapter, n_estimators: [int] = None, max_features: [int] = None):
-        self._n_estimators = n_estimators or [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+        self._n_estimators = n_estimators or [50, 100, 150, 200, 250, 300, 350, 400]
         self._max_features = max_features or [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         self._machine_learning_adapter = machine_learning_adapter
 
